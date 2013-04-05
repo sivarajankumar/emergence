018bf093f5a65263db9e253456a7f45c42d513bd
jorvis <jorvis@976c60d5-a951-075d-e5ae-1b3cfda58035>
2013-04-05 05:26:18 +0000

    Added initial project directory structure

---
+++ trunk/README.txt
@@ -0,0 +1,13 @@
+=========
+Emergence
+=========
+
+Documentation stump while working out the basic distribution and project structure.
+
+This should be written in reStructuredText:
+
+http://docutils.sourceforge.net/rst.html
+
+Example:
+
+http://guide.python-distribute.org/creation.html#basics-creating-and-distributing-distributions
---
+++ trunk/setup.py
@@ -0,0 +1,16 @@
+from distutils.core import setup
+
+setup(
+    name='Emergence',
+    version='0.0.0',
+    author='Joshua Orvis',
+    author_email='jorvis@gmail.com',
+    packages=['emergence', 'emergence.test'],
+    scripts=[],
+    url='http://code.google.com/p/emergence/',
+    license='LICENSE.txt',
+    description='A biological sequence annotation and analysis system.',
+    long_description=open('README.txt').read(),
+    install_requires=[
+        "Django >= 1.5.1",
+)
